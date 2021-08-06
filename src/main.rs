c6ae834b84a2f92be466c615425117e74b8b0a23
Andrea Cosentino <ancosen@gmail.com>
2021-08-06 10:55:48 +0200

    Added the option to specify the worksheet name

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::process;
 pub fn convert_csv_to_excel(
     csv_path: &str,
     excel_path: &str,
+    sheet_name: &str
 ) -> Result<(), Box<dyn std::error::Error>> {
     let mut rdr = csv::ReaderBuilder::new()
         .has_headers(false)
@@ -10,8 +11,8 @@ pub fn convert_csv_to_excel(
         .from_path(csv_path)?;
 
     let mut wb = simple_excel_writer::Workbook::create(excel_path);
-    let mut sheet = wb.create_sheet("Sheet 1");
-    
+    let mut sheet = wb.create_sheet(&mut &sheet_name);
+
     wb.write_sheet(&mut sheet, |sw| {
         for result in rdr.records() {
             if let Ok(record) = result {
@@ -35,5 +36,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("A CSV file as input and an output file must be provided");
         process::exit(1)
     } ;
-    convert_csv_to_excel(&args[1], &args[2])
+    let mut sheet_name = "Worksheet";
+    if args.len() >= 4 {
+        sheet_name = &args[3];
+    }
+    convert_csv_to_excel(&args[1], &args[2], &mut &sheet_name)
 }
